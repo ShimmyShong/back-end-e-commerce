4c7839c9ba91a9837534f23116ad370854e80263
ShimmyShong <lattabun@gmail.com>
2023-05-17 14:41:28 -0400

    defining category and product models;

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,8 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category)
 
 // Categories have many Products
+Categories.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
 
